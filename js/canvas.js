deda447ac32678aed5de174c9eeaece67b62ada0
forsmarts <forsmarts@gmail.com>
2016-05-02 14:53:04 +0300

    - Slightly reformatted; - Grid drawing bug fixed; - One new level added

--- js/canvas.js
+++ js/canvas.js
@@ -4,194 +4,194 @@ var thisCell = new cBandsCell();
 var newCell = new cBandsCell();
 var old_dy = old_dx = 0;
 
-var move = function(dx,dy) {
-	var dirVer = canvas.direction[thisCell.direction]%2 == 0;
-	var dirHor = canvas.direction[thisCell.direction]%2 == 1;
-	var dirVerCorner = thisCell.isCorner && canvas.direction[thisCell.prevDirection]%2 == 0;
-	var dirHorCorner = thisCell.isCorner && canvas.direction[thisCell.prevDirection]%2 == 1;
-	var coeff;
-	
-	if (dirVer || dirVerCorner) {
-		if (dirVer) {
-			coeff = -(canvas.direction[thisCell.direction]-1);
-		} else {
-			coeff = -(canvas.direction[thisCell.prevDirection]-1);
-		}
-		if (coeff*(dy - old_dy) > canvas.cellSize / 2)  {
-			var aColors = [];
-			for (i=0; i <= thisCell.bandSize-1; i++) {
-				aColors.push(thisCell.prev.color);
-				thisCell = thisCell.prev;
-			}
-			for (i=0; i <= thisCell.bandSize-1; i++) {
-				thisCell.color = aColors[i];
-				thisCell.element.attr({fill: canvas.colors[aColors[i]]});
-				thisCell = thisCell.prev;				
-			}
-			old_dy = old_dy + coeff * canvas.cellSize;
-		}
-		if (-coeff*(dy - old_dy) > canvas.cellSize / 2)  { 
-			var aColors = [];
-			for (i=0; i <= thisCell.bandSize-1; i++) {
-				aColors.push(thisCell.next.color);
-				thisCell = thisCell.next;
-			}
-			for (i=0; i <= thisCell.bandSize-1; i++) {
-				thisCell.color = aColors[i];
-				thisCell.element.attr({fill: canvas.colors[aColors[i]]});
-				thisCell = thisCell.next;				
-			}
-			old_dy = old_dy - coeff * canvas.cellSize;
-		}
-	}
-	if (dirHor || dirHorCorner) {
-		if (dirHor) {
-			coeff = -(canvas.direction[thisCell.direction]-2);
-		} else {
-			coeff = -(canvas.direction[thisCell.prevDirection]-2);
-		}
-		if (coeff*(dx - old_dx) > canvas.cellSize / 2)  {
-			var aColors = [];
-			for (i=0; i <= thisCell.bandSize-1; i++) {
-				aColors.push(thisCell.prev.color);
-				thisCell = thisCell.prev;
-			}
-			for (i=0; i <= thisCell.bandSize-1; i++) {
-				thisCell.color = aColors[i];
-				thisCell.element.attr({fill: canvas.colors[aColors[i]]});
-				thisCell = thisCell.prev;				
-			}
-			old_dx = old_dx + coeff * canvas.cellSize;
-		}
-		if (-coeff*(dx - old_dx) > canvas.cellSize / 2)  {
-			var aColors = [];
-			for (i=0; i <= thisCell.bandSize-1; i++) {
-				aColors.push(thisCell.next.color);
-				thisCell = thisCell.next;
-			}
-			for (i=0; i <= thisCell.bandSize-1; i++) {
-				thisCell.color = aColors[i];
-				thisCell.element.attr({fill: canvas.colors[aColors[i]]});
-				thisCell = thisCell.next;				
-			}
-			old_dx = old_dx - coeff * canvas.cellSize;
-		}
-	}
-	if (canvas.puzzle.isSolved()) {
-		canvas.puzzle.element.attr({fill: "#f9f"});
-		$("#btnNextPuzzle").show();
-	}
+var move = function (dx, dy) {
+    var dirVer = canvas.direction[thisCell.direction] % 2 == 0;
+    var dirHor = canvas.direction[thisCell.direction] % 2 == 1;
+    var dirVerCorner = thisCell.isCorner && canvas.direction[thisCell.prevDirection] % 2 == 0;
+    var dirHorCorner = thisCell.isCorner && canvas.direction[thisCell.prevDirection] % 2 == 1;
+    var coeff;
+
+    if (dirVer || dirVerCorner) {
+        if (dirVer) {
+            coeff = -(canvas.direction[thisCell.direction] - 1);
+        } else {
+            coeff = -(canvas.direction[thisCell.prevDirection] - 1);
+        }
+        if (coeff * (dy - old_dy) > canvas.cellSize / 2) {
+            var aColors = [];
+            for (i = 0; i <= thisCell.bandSize - 1; i++) {
+                aColors.push(thisCell.prev.color);
+                thisCell = thisCell.prev;
+            }
+            for (i = 0; i <= thisCell.bandSize - 1; i++) {
+                thisCell.color = aColors[i];
+                thisCell.element.attr({ fill: canvas.colors[aColors[i]] });
+                thisCell = thisCell.prev;
+            }
+            old_dy = old_dy + coeff * canvas.cellSize;
+        }
+        if (-coeff * (dy - old_dy) > canvas.cellSize / 2) {
+            var aColors = [];
+            for (i = 0; i <= thisCell.bandSize - 1; i++) {
+                aColors.push(thisCell.next.color);
+                thisCell = thisCell.next;
+            }
+            for (i = 0; i <= thisCell.bandSize - 1; i++) {
+                thisCell.color = aColors[i];
+                thisCell.element.attr({ fill: canvas.colors[aColors[i]] });
+                thisCell = thisCell.next;
+            }
+            old_dy = old_dy - coeff * canvas.cellSize;
+        }
+    }
+    if (dirHor || dirHorCorner) {
+        if (dirHor) {
+            coeff = -(canvas.direction[thisCell.direction] - 2);
+        } else {
+            coeff = -(canvas.direction[thisCell.prevDirection] - 2);
+        }
+        if (coeff * (dx - old_dx) > canvas.cellSize / 2) {
+            var aColors = [];
+            for (i = 0; i <= thisCell.bandSize - 1; i++) {
+                aColors.push(thisCell.prev.color);
+                thisCell = thisCell.prev;
+            }
+            for (i = 0; i <= thisCell.bandSize - 1; i++) {
+                thisCell.color = aColors[i];
+                thisCell.element.attr({ fill: canvas.colors[aColors[i]] });
+                thisCell = thisCell.prev;
+            }
+            old_dx = old_dx + coeff * canvas.cellSize;
+        }
+        if (-coeff * (dx - old_dx) > canvas.cellSize / 2) {
+            var aColors = [];
+            for (i = 0; i <= thisCell.bandSize - 1; i++) {
+                aColors.push(thisCell.next.color);
+                thisCell = thisCell.next;
+            }
+            for (i = 0; i <= thisCell.bandSize - 1; i++) {
+                thisCell.color = aColors[i];
+                thisCell.element.attr({ fill: canvas.colors[aColors[i]] });
+                thisCell = thisCell.next;
+            }
+            old_dx = old_dx - coeff * canvas.cellSize;
+        }
+    }
+    if (canvas.puzzle.isSolved()) {
+        canvas.puzzle.element.attr({ fill: "#f9f" });
+        $("#btnNextPuzzle").show();
+    }
 }
 
 var start = function (cx, cy) {
-	old_dy = old_dx = 0;
-	var x = Math.floor((cx - mainGrid.getBoundingClientRect().left) / canvas.cellSize);
-	var y = Math.floor((cy - mainGrid.getBoundingClientRect().top) / canvas.cellSize);
-	thisCell = canvas.puzzle.cells[x][y];
-	this.data('origTransform', this.transform().local );
+    old_dy = old_dx = 0;
+    var x = Math.floor((cx - mainGrid.getBoundingClientRect().left) / canvas.cellSize);
+    var y = Math.floor((cy - mainGrid.getBoundingClientRect().top) / canvas.cellSize);
+    thisCell = canvas.puzzle.cells[x][y];
+    this.data('origTransform', this.transform().local);
 }
-var stop = function() {
-}
-
-cBandsCanvas = function(puzzle){
-  this.puzzle = puzzle;
-  this.colors = {
-    "1": "#e55",
-    "2": "#5e5",
-    "3": "#55e",
-    "4": "#c5c",
-    "5": "#f95",
-    "6": "#ff0"
-  }
-  // Sides and their opposites.
-  this.opposite = {
-    "L" : "R",
-    "R" : "L",
-    "U" : "D",
-    "D" : "U"
-  }
-  this.direction = {
-    "D" : 0,
-    "U" : 2,
-    "R" : 1,
-    "L" : 3
-  }
+var stop = function () {
 }
 
-cBandsCanvas.prototype.render = function(snap){
-  this.snap = snap;
-  this.cellSize = Math.min(snap.node.clientHeight/this.puzzle.gridSize, snap.node.clientWidth/this.puzzle.gridSize);
-  this.ballSize = this.cellSize / 3;
-  this.puzzle.element = this.drawBoard(this.puzzle.gridSize);
-  for (var i = 0; i < this.puzzle.gridSize; i++) {
-
-    for (var j = 0; j < this.puzzle.gridSize; j++) {
-
-      this.drawCell(this.puzzle.cells[i][j]);
+cBandsCanvas = function (puzzle) {
+    this.puzzle = puzzle;
+    this.colors = {
+        "1": "#e55",
+        "2": "#5e5",
+        "3": "#55e",
+        "4": "#c5c",
+        "5": "#f95",
+        "6": "#ff0"
     }
-  }
-}
-
-cBandsCanvas.prototype.drawBoard = function(gridSize) {
-  var board = this.snap.rect(0, 0, gridSize * this.cellSize,gridSize * this.cellSize);
-  board.attr({
-    fill: "#fff",
-    stroke: "#000",
-    strokeWidth: 6
-  })
-  board.drag(move,start,stop);
-  return board;
-}
-
-cBandsCanvas.prototype.drawCell = function(cell){
-  // The center of the cell
-  position = {
-    x: cell.column * this.cellSize + this.cellSize/2,
-    y: cell.row * this.cellSize + this.cellSize/2
-  }
-  var lines = []
-  for (side in this.opposite) {
-    if (cell.direction != side && cell.prevDirection != this.opposite[side]) {
-      lines.push(this.drawInnerBorder( position, side));
+    // Sides and their opposites.
+    this.opposite = {
+        "L": "R",
+        "R": "L",
+        "U": "D",
+        "D": "U"
     }
-  }
-  cell.lines = lines;
-  cell.element = this.drawBall(position, this.colors[cell.color]);
+    this.direction = {
+        "D": 0,
+        "U": 2,
+        "R": 1,
+        "L": 3
+    }
 }
 
-cBandsCanvas.prototype.drawInnerBorder = function(position, side){
-  if (side == 'U'){
-    var start = {x: -1, y: -1};
-    var end = {x: 1, y: -1};
-  }
-  if (side == 'D'){
-    var start = {x: -1, y: 1};
-    var end = {x: 1, y: 1};
-  }
-  if (side == 'L'){
-    var start = {x: -1, y: -1};
-    var end = {x: -1, y: 1};
-  }
-  if (side == 'R'){
-    var start = {x: 1, y: -1};
-    var end = {x: 1, y: 1};
-  }
-  var line = this.snap.line(
-    position.x + start.x * this.cellSize/2, position.y + start.y * this.cellSize/2,
-    position.x + end.x * this.cellSize/2, position.y + end.y * this.cellSize/2);
-  line.attr({
-    stroke: "#000",
-    strokeWidth: 2
-  });
+cBandsCanvas.prototype.render = function (snap) {
+    this.snap = snap;
+    this.cellSize = Math.min(snap.node.clientHeight / this.puzzle.gridSize, snap.node.clientWidth / this.puzzle.gridSize);
+    this.ballSize = this.cellSize / 3;
+    this.puzzle.element = this.drawBoard(this.puzzle.gridSize);
+    for (var i = 0; i < this.puzzle.gridSize; i++) {
+
+        for (var j = 0; j < this.puzzle.gridSize; j++) {
+
+            this.drawCell(this.puzzle.cells[i][j]);
+        }
+    }
 }
 
-cBandsCanvas.prototype.drawBall = function(position, color) {
-  var circle = this.snap.circle(position.x, position.y, this.ballSize);
-  circle.attr({
-    fill: color,
-    stroke: "#000",
-    strokeWidth: 1
-  })
-  circle.drag(move,start,stop);
-  return circle;
+cBandsCanvas.prototype.drawBoard = function (gridSize) {
+    var board = this.snap.rect(0, 0, gridSize * this.cellSize, gridSize * this.cellSize);
+    board.attr({
+        fill: "#fff",
+        stroke: "#000",
+        strokeWidth: 6
+    })
+    board.drag(move, start, stop);
+    return board;
+}
+
+cBandsCanvas.prototype.drawCell = function (cell) {
+    // The center of the cell
+    position = {
+        x: cell.column * this.cellSize + this.cellSize / 2,
+        y: cell.row * this.cellSize + this.cellSize / 2
+    }
+    var lines = []
+    for (side in this.opposite) {
+        if ((cell.direction != side || cell.isLast) && cell.prevDirection != this.opposite[side]) {
+            lines.push(this.drawInnerBorder(position, side));
+        }
+    }
+    cell.lines = lines;
+    cell.element = this.drawBall(position, this.colors[cell.color]);
+}
+
+cBandsCanvas.prototype.drawInnerBorder = function (position, side) {
+    if (side == 'U') {
+        var start = { x: -1, y: -1 };
+        var end = { x: 1, y: -1 };
+    }
+    if (side == 'D') {
+        var start = { x: -1, y: 1 };
+        var end = { x: 1, y: 1 };
+    }
+    if (side == 'L') {
+        var start = { x: -1, y: -1 };
+        var end = { x: -1, y: 1 };
+    }
+    if (side == 'R') {
+        var start = { x: 1, y: -1 };
+        var end = { x: 1, y: 1 };
+    }
+    var line = this.snap.line(
+      position.x + start.x * this.cellSize / 2, position.y + start.y * this.cellSize / 2,
+      position.x + end.x * this.cellSize / 2, position.y + end.y * this.cellSize / 2);
+    line.attr({
+        stroke: "#000",
+        strokeWidth: 2
+    });
+}
+
+cBandsCanvas.prototype.drawBall = function (position, color) {
+    var circle = this.snap.circle(position.x, position.y, this.ballSize);
+    circle.attr({
+        fill: color,
+        stroke: "#000",
+        strokeWidth: 1
+    })
+    circle.drag(move, start, stop);
+    return circle;
 }
--- js/puzzle.js
+++ js/puzzle.js
@@ -1,120 +1,123 @@
 // Object which represents the puzzle state
 
-var cBandsPuzzle = function(data){
-  this.data = data;
-  this.parseData(data);
+var cBandsPuzzle = function (data) {
+    this.data = data;
+    this.parseData(data);
 }
 
-cBandsPuzzle.prototype.parseData = function(data){
-  this.gridSize = data.gridSize;
-  var cells = [];
-  // create all cells
-  for (var i = 0; i < this.gridSize; i++) {
+cBandsPuzzle.prototype.parseData = function (data) {
+    this.gridSize = data.gridSize;
+    var cells = [];
+    // create all cells
+    for (var i = 0; i < this.gridSize; i++) {
 
-    cells[i] = new Array(this.gridSize);
+        cells[i] = new Array(this.gridSize);
 
-    for (var j = 0; j < this.gridSize; j++) {
+        for (var j = 0; j < this.gridSize; j++) {
 
-      cells[i][j] = new cBandsCell(i, j);
+            cells[i][j] = new cBandsCell(i, j);
+        }
     }
-  }
-  // parse bands
-  for(var i=0; i < data.bands.length; i++){
-    this.parseBand(data.bands[i], cells);
-  }
-  this.cells = cells;
+    // parse bands
+    for (var i = 0; i < data.bands.length; i++) {
+        this.parseBand(data.bands[i], cells);
+    }
+    this.cells = cells;
 }
 
-cBandsPuzzle.prototype.parseBand = function(band, cells) {
-  var  getBandSize = function(bandString) {
-    return bandString.replace(/[DLRU]/g,"").length;
-  }
-  var shiftTo = function(position, direction){
-    if (direction=='R'){
-      position.x++;
+cBandsPuzzle.prototype.parseBand = function (band, cells) {
+    var getBandSize = function (bandString) {
+        return bandString.replace(/[DLRU]/g, "").length;
     }
-    if (direction=='D'){
-      position.y++;
+    var shiftTo = function (position, direction) {
+        if (direction == 'R') {
+            position.x++;
+        }
+        if (direction == 'D') {
+            position.y++;
+        }
+        if (direction == 'U') {
+            position.y--;
+        }
+        if (direction == 'L') {
+            position.x--;
+        }
     }
-    if (direction=='U'){
-      position.y--;
+    var position = {
+        x: Number(band.charAt(0)),
+        y: Number(band.charAt(2))
     }
-    if (direction=='L'){
-      position.x--;
+    var firstCell = cells[position.x][position.y];
+    var prevCell = undefined;
+    var direction = 'R';
+    // Remove to digits, comma and colon.
+    band = band.slice(4);
+    var bandSize = getBandSize(band);
+    // Looping for whole band.
+    while (band.length > 0) {
+        var cell = cells[position.x][position.y];
+        cell.bandSize = bandSize;
+        // Set color
+        var color = Number(band.charAt(0));
+        cell.color = color;
+        band = band.slice(1);
+        // Change direction if needed.
+        var nextLetter = band.charAt(0);
+        if (nextLetter == 'R' || nextLetter == 'D' || nextLetter == 'U' || nextLetter == 'L') {
+            direction = nextLetter;
+            cell.isCorner = true;
+            band = band.slice(1);
+        }
+        // Move to the next cell.
+        shiftTo(position, direction);
+        if (prevCell) {
+            cell.prevDirection = prevCell.direction;
+            cell.prev = prevCell;
+            prevCell.next = cell;
+        }
+        // Remember the direction to draw special edge there.
+        if (band.length >= 0) {
+            cell.direction = direction;
+        }
+        if (band.length == 0) {
+            cell.isLast = true;
+        }
+        prevCell = cell;
     }
-  }
-  var position = {
-    x: Number(band.charAt(0)),
-    y: Number(band.charAt(2))
-  }
-  var firstCell = cells[position.x][position.y];
-  var prevCell = undefined;
-  var direction = 'R';
-  // Remove to digits, comma and colon.
-  band = band.slice(4);
-  var bandSize = getBandSize(band);
-  // Looping for whole band.
-  while (band.length > 0){
-    var cell = cells[position.x][position.y];
-  cell.bandSize = bandSize;
-    // Set color
-    var color = Number(band.charAt(0));
-    cell.color = color;
-    band = band.slice(1);
-    // Change direction if needed.
-    var nextLetter = band.charAt(0);
-    if (nextLetter=='R' || nextLetter=='D' || nextLetter=='U' || nextLetter=='L' ){
-      direction = nextLetter;
-	  cell.isCorner = true;
-      band = band.slice(1);
-    }
-    // Move to the next cell.
-    shiftTo(position, direction);
-    if (prevCell) {
-      cell.prevDirection = prevCell.direction;
-      cell.prev = prevCell;
-      prevCell.next = cell;
-    }
-    // Remember the direction to draw special edge there.
-    if (band.length >= 0) {
-      cell.direction = direction;
-    }
-    prevCell = cell;
-  }
-  firstCell.prev = prevCell;
-  prevCell.next = firstCell;
+    firstCell.prev = prevCell;
+    prevCell.next = firstCell;
 }
 
-cBandsPuzzle.prototype.isSolved = function() {
-  var aArray = [];
+cBandsPuzzle.prototype.isSolved = function () {
+    var aArray = [];
 
-  var isAllDifferent = function(aArray) {
-    var counter = {};
-    for (i=0; i<aArray.length; i++) {
-      if(counter[aArray[i]] != undefined) {
-        return false;
-      }
-      counter[aArray[i]] = 1;
+    var isAllDifferent = function (aArray) {
+        var counter = {};
+        for (i = 0; i < aArray.length; i++) {
+            if (counter[aArray[i]] != undefined) {
+                return false;
+            }
+            counter[aArray[i]] = 1;
+        }
+        return true;
+    }
+    for (x = 0; x <= this.gridSize - 1; x++) {
+        aArray = [];
+        for (y = 0; y <= this.gridSize - 1; y++) {
+            aArray.push(this.cells[x][y].color);
+        }
+        if (!isAllDifferent(aArray)) {
+            return false;
+        }
+    }
+    for (y = 0; y <= this.gridSize - 1; y++) {
+        aArray = [];
+        for (x = 0; x <= this.gridSize - 1; x++) {
+            aArray.push(this.cells[x][y].color);
+        }
+        if (!isAllDifferent(aArray)) {
+            return false;
+        }
     }
     return true;
-  }
-  for (x = 0; x <= this.gridSize-1; x++) {
-    aArray = [];
-    for (y = 0; y <= this.gridSize-1; y++) {
-      aArray.push(this.cells[x][y].color);
-    }
-    if (!isAllDifferent(aArray)) {
-      return false;
-    }
-  }
-  for (y = 0; y <= this.gridSize-1; y++) {
-    aArray = [];
-    for (x = 0; x <= this.gridSize-1; x++) {
-      aArray.push(this.cells[x][y].color);
-    }
-    if (!isAllDifferent(aArray)) {
-      return false;
-    }
-  }
-  return true;
 }
